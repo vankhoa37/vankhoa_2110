18f7899770cdda919787306507442280e2da137a
vankhoa37 <161930087+vankhoa37@users.noreply.github.com>
2024-03-11 23:04:51 +0700

    Add files via upload

--- test_6_2.cpp
+++ test_6_2.cpp
@@ -10,7 +10,8 @@ int initialMouseY = 0;
 int size = 56;
 SDL_Rect figures[32];
 string position = "";
-int board[8][8] = {
+int board[8][8] =
+{
     {-1, -2, -3, -4, -5, -3, -2, -1},
     {-6, -6, -6, -6, -6, -6, -6, -6},
     {0, 0, 0, 0, 0, 0, 0, 0},
@@ -30,22 +31,28 @@ SDL_Texture* gChessTexture = NULL;
 int selectedPieceIndex = -1;
 bool quit;
 bool isMousePressed = false;
-bool isPositionInsideRect(int x, int y, const SDL_Rect& rect) {
+bool isPositionInsideRect(int x, int y, const SDL_Rect& rect)
+{
     return (x >= rect.x && x < rect.x + rect.w && y >= rect.y && y < rect.y + rect.h);
 }
-void handlePieceMovement(int mouseX, int mouseY) {
-    if (selectedPieceIndex != -1) {
+void handlePieceMovement(int mouseX, int mouseY)
+{
+    if (selectedPieceIndex != -1)
+        {
         figures[selectedPieceIndex].x = mouseX - size / 2;
         figures[selectedPieceIndex].y = mouseY - size / 2;
-    }
+        }
 }
-bool isValidMove(int startRow, int startCol, int targetRow, int targetCol) {
-    return (targetRow >= 0 && targetRow < 8 && targetCol >= 0 && targetCol < 8);
+bool isValidMove(int startRow, int startCol, int targetRow, int targetCol)
+{
+    return (targetRow >= 0 && targetRow < 8 && targetCol >= 0 && targetCol < 8 && (targetRow != startRow || targetCol != startCol));
 }
-void loadPosition() {
+void loadPosition()
+{
     int k = 0;
     for (int i = 0; i < 8; i++)
-        for (int j = 0; j < 8; j++) {
+        for (int j = 0; j < 8; j++)
+            {
             int n = board[i][j];
             if (!n) continue;
             int chessWidth = 56;
@@ -61,9 +68,10 @@ void loadPosition() {
             SDL_Rect sourceRect = {chessX, chessY, chessWidth, chessHeight};
             SDL_RenderCopy(renderer, gChessTexture, &sourceRect, &figures[k]);
             k++;
-        }
+           }
 }
-void stockfishMove(const std::string move) {
+void stockfishMove(const std::string move)
+{
     int X1 = int(move[0]) - 97;
     int Y1 = (move[1] - '0') - 1;
     int X2 = int(move[2]) - 97;
@@ -77,8 +85,42 @@ void stockfishMove(const std::string move) {
     //cout << board[Y1][X1];
     std::cout << " doi nuoc thanh cong  " << endl;
 }
+bool isCastling (const string a)
+{
+    int X1 = int(a[0]) - 97;
+    int Y1 = (a[1] - '0') - 1;
+    int X2 = int(a[2]) - 97;
+    int Y2 = (a[3] - '0') - 1;
+    //cout << board[Y1][X1] << board[X1][Y1] << board[X2][Y2] << board[Y2][X2] << endl;
+    int king = abs(board[Y1][X1]);
+    for(int i = 0; i < 8; i++)
+    {
+        for(int j = 0; j < 8; j++)
+        {
+            cout << board[i][j] << " ";
+        }
+        cout << endl;
+    }
+    std::cout << king << std::endl;
+    if ((a == "e1g1" || a == "e8c8" || a == "e1c1" || a == "e8g8") && king == 5)
+    {
+        std::cout << "la nhap thanh ";
+        return true;
+    }
+        return false;
+}
+void castling (string a)
+{
+    if (a == "e1g1") stockfishMove("h1f1");
+    if (a == "e8c8") stockfishMove("a8d8");
+    if (a == "e1c1") stockfishMove("a1d1");
+    if (a == "e8g8") stockfishMove("h8f8");
 
-int main(int argc, char* argv[]) {
+}
+
+
+int main(int argc, char* argv[])
+{
     SDL_Init(SDL_INIT_EVERYTHING);
     window = SDL_CreateWindow("The Chess", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 453 + 46, 453 + 46, SDL_WINDOW_SHOWN);
     renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
@@ -90,7 +132,8 @@ int main(int argc, char* argv[]) {
     bool quit = false;
     int val = 1;
 
-    while (!quit) {
+    while (!quit)
+        {
         SDL_Event event;
         string Move = "";
         string check = " ";
@@ -99,17 +142,28 @@ int main(int argc, char* argv[]) {
                 string str = getNextMove(position);
                 position += str + " ";
                 cout << str;
+                if (isCastling(str))
+                {
+                    castling(str);
+                }
                 stockfishMove(str);
                 val --;
 
             }
-        while (SDL_PollEvent(&event) != 0) {
-            if (event.type == SDL_QUIT) {
+        while (SDL_PollEvent(&event) != 0)
+            {
+            if (event.type == SDL_QUIT)
+             {
                 quit = true;
-            } else if (event.type == SDL_MOUSEBUTTONDOWN) {
-                if (event.button.button == SDL_BUTTON_LEFT) {
-                    for (int i = 0; i < 32; i++) {
-                        if (isPositionInsideRect(event.button.x, event.button.y, figures[i])) {
+             }
+            else if (event.type == SDL_MOUSEBUTTONDOWN)
+            {
+                if (event.button.button == SDL_BUTTON_LEFT)
+                {
+                    for (int i = 0; i < 32; i++)
+                        {
+                        if (isPositionInsideRect(event.button.x, event.button.y, figures[i]))
+                        {
                             selectedPieceIndex = i;
                             initialMouseX = event.button.x;
                             initialMouseY = event.button.y;
@@ -117,9 +171,11 @@ int main(int argc, char* argv[]) {
                         }
                     }
                 }
-            } else if (event.type == SDL_MOUSEBUTTONUP) {
+            } else if (event.type == SDL_MOUSEBUTTONUP)
+            {
                 val++;
-                if (event.button.button == SDL_BUTTON_LEFT) {
+                if (event.button.button == SDL_BUTTON_LEFT)
+                    {
                     if (selectedPieceIndex != -1) {
                         for(int i = 0; i < 32 ; i++)
                            if(isPositionInsideRect(event.button.x, event.button.y, figures[i]))
@@ -128,9 +184,9 @@ int main(int argc, char* argv[]) {
                         int startRow = (initialMouseY - 23) / size;
                         int targetCol = (event.button.x - 23) / size;
                         int targetRow = (event.button.y - 23) / size;
-                        if (isValidMove(startRow, startCol, targetRow, targetCol)) {
-                            board[targetRow][targetCol] = board[startRow][startCol];
-                            board[startRow][startCol] = 0;
+                        if (isValidMove(startRow, startCol, targetRow, targetCol))
+                        {
+
                            // Move += chess[abs(board[targetRow][targetCol])];
                             //Move += check;
                             Move += row[startCol];
@@ -139,12 +195,19 @@ int main(int argc, char* argv[]) {
                             Move += col[targetRow];
                             //cout << Move << endl;
                             cout << Move << endl;
+                            if(isCastling(Move))
+                            {
+                                castling(Move);
+                            }
+                            board[targetRow][targetCol] = board[startRow][startCol];
+                            board[startRow][startCol] = 0;
                             position += Move + " ";
                         }
                         selectedPieceIndex = -1;
                     }
                 }
-            } else if (event.type == SDL_MOUSEMOTION) {
+            } else if (event.type == SDL_MOUSEMOTION)
+            {
                 handlePieceMovement(event.motion.x, event.motion.y);
             }
         }
@@ -162,3 +225,5 @@ int main(int argc, char* argv[]) {
 
     return 0;
 }
+// update nhap thanh
+// update promote
